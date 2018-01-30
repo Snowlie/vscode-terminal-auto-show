9698066675119f428f7224d29e3a6a591c2d048b
Snowfall <eagle2369@hotmail.com>
2018-01-30 13:38:30 -0500

    Init
    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+All notable changes to the "terminalautoshow" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [0.0.1] - 2018-01-29
+Initial release
+
+## [0.0.2] - 2018-01-30
+Fix for terminal not showing properly
+
+## [0.0.5] - 2018-01-30
+Updated package settings
---
+++ src/extension.ts
@@ -0,0 +1,94 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { Disposable } from 'vscode';
+import { window } from 'vscode';
+import { workspace } from 'vscode';
+import { Terminal } from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "terminalautoshow" is now active!');
+
+    let autoShow = new TerminalAutoShow();
+    let autoShowControl = new TerminalAutoShowController(autoShow);
+
+    context.subscriptions.push(autoShow);
+    context.subscriptions.push(autoShowControl);
+}
+
+class TerminalAutoShow {
+    private _terminal: Terminal;
+    private _hidden = false;
+
+    constructor() {
+        this._terminal = window.createTerminal();
+    }
+
+    doucmentClosed() {
+        console.log("Doc closed");
+        console.log(workspace.textDocuments);
+        if (window.visibleTextEditors.length <= 0) {
+            this.showTerminal();
+        }
+    }
+
+    documentOpened() {
+        console.log("Doc opened");
+        this.hideTerminal();
+    }
+
+    showTerminal() {
+        this._terminal.show();
+        this._hidden = false;
+    }
+
+    hideTerminal() {
+        this._terminal.hide();
+        this._hidden = true;
+    }
+
+    fullScreenTerminal() {
+        this._terminal.show();
+    }
+
+    dispose() {
+        this._terminal.dispose();
+    }
+}
+
+class TerminalAutoShowController {
+    private _autoShow: TerminalAutoShow;
+    private _disposable: Disposable;
+
+    constructor(autoShow: TerminalAutoShow) {
+        this._autoShow = autoShow;
+
+        let subscriptions: Disposable[] = [];
+        workspace.onDidCloseTextDocument(this._documentClosed, this, subscriptions);
+        workspace.onDidOpenTextDocument(this._documentOpened, this, subscriptions);
+
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    _documentClosed() {
+        this._autoShow.doucmentClosed();
+    }
+
+    _documentOpened() {
+        this._autoShow.documentOpened();
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
